63ed57981a8d92f46e12688b8359a0987d0fb1dd
FuturisticSpaceCake <thomas.c.hoek@gmail.com>
2020-05-08 16:49:23 +0200

    additions to animation after painting game. Almost bug free and finished.

--- js/main.js
+++ js/main.js
@@ -28,7 +28,7 @@ back_button.addEventListener('click', () => {
 
 let scan_button = document.getElementById("scan_image");
 scan_button.addEventListener('click', function() {
-	UpdateAppState(states.Video);
+	UpdateAppState(states.PaintingCharacterSelection);
 });
 
 let instructions_button = document.getElementById("help_needed");
--- js/modules/playPaintedCharacterAnimationSceneObject.js
+++ js/modules/playPaintedCharacterAnimationSceneObject.js
@@ -1,62 +1,118 @@
-// import { loadingManager } from './contentLoader.js';
+import { FBXLoader } from '../vendor/FBXLoader.js';
+import { loadingManager } from './contentLoader.js';
 
-// const characterAnimationSceneObject = {
-// 	camera: new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 1000 ),
-// 	scene: new THREE.Scene(),
-// 	animationComplete: null,
+const paintedCharacterAnimation = {
+	camera: new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 1000 ),
+	scene: new THREE.Scene(),
 	
-// 	clock: new THREE.Clock(),
-// 	animatedModels: [],
+	character_files: [
+		"Baseball_Umpire",
+		"Goalkeeper_Catch"
+	],
+	characters: [],
+	active_character: null,
+	clock: new THREE.Clock(),
+	animationLoopCompleteCallback: null,
 
-// 	init: function() {
+	init: function() {
 
-// 		this.camera.position.set( 0, 2, -5 );
-// 		this.camera.lookAt( 0, 1, 0 );
+		this.camera.position.set( 0, 2, -5 );
+		this.camera.lookAt( 0, 1, 0 );
 
-// 		this.scene.background = new THREE.Color( 0xa0a0a0 );
-// 		this.scene.fog = new THREE.Fog( 0xa0a0a0, 10, 50 );
+		this.scene.background = new THREE.Color( 0xa0a0a0 );
+		this.scene.fog = new THREE.Fog( 0xa0a0a0, 10, 50 );
 
-// 		var hemiLight = new THREE.HemisphereLight( 0xffffff, 0x444444 );
-// 		hemiLight.position.set( 0, 20, 0 );
-// 		this.scene.add( hemiLight );
+		var hemiLight = new THREE.HemisphereLight( 0xffffff, 0x444444 );
+		hemiLight.position.set( 0, 20, 0 );
+		this.scene.add( hemiLight );
 
-// 		var dirLight = new THREE.DirectionalLight( 0xffffff );
-// 		dirLight.position.set( - 3, 10, - 10 );
-// 		dirLight.castShadow = true;
-// 		dirLight.shadow.camera.top = 2;
-// 		dirLight.shadow.camera.bottom = - 2;
-// 		dirLight.shadow.camera.left = - 2;
-// 		dirLight.shadow.camera.right = 2;
-// 		dirLight.shadow.camera.near = 0.1;
-// 		dirLight.shadow.camera.far = 40;
-// 		this.scene.add( dirLight );
+		var dirLight = new THREE.DirectionalLight( 0xffffff );
+		dirLight.position.set( - 3, 10, - 10 );
+		dirLight.castShadow = true;
+		dirLight.shadow.camera.top = 2;
+		dirLight.shadow.camera.bottom = - 2;
+		dirLight.shadow.camera.left = - 2;
+		dirLight.shadow.camera.right = 2;
+		dirLight.shadow.camera.near = 0.1;
+		dirLight.shadow.camera.far = 40;
+		this.scene.add( dirLight );
 
-// 		var mesh = new THREE.Mesh( new THREE.PlaneBufferGeometry( 100, 100 ), new THREE.MeshPhongMaterial( { color: 0x999999, depthWrite: false } ) );
-// 		mesh.rotation.x = - Math.PI / 2;
-// 		mesh.receiveShadow = true;
-// 		this.scene.add( mesh );
-// 	},
+		var mesh = new THREE.Mesh( new THREE.PlaneBufferGeometry( 100, 100 ), new THREE.MeshPhongMaterial( { color: 0x999999, depthWrite: false } ) );
+		mesh.rotation.x = - Math.PI / 2;
+		mesh.receiveShadow = true;
+		this.scene.add( mesh );
+	},
 
-// 	loadResources: function() {
+	loadResources: function() {
+		var ref = characterAnimationSceneObject;
+		var loader = new FBXLoader();
 
-// 	},
+		this.character_files.forEach(character_file => {
+			loader.load('./Data/Models/Animations/'+character_file+'.fbx', function ( fbx ) {
 
-// 	play: function(callback) {
-// 		this.animationComplete = callback;
-// 		this.playAnimation = true;
-// 	},
+				fbx.rotation.y = Math.PI;
+				fbx.scale.set(0.01, 0.01, 0.01);
 
-// 	animate: function() {
-// 		if(this.modelLoaded && this.playAnimation) {
-// 			this.walkAnimation.play();	
-// 			this.playAnimation = false;
-// 		}
-// 		var mixerUpdateDelta = this.clock.getDelta();
-// 		if(this.animationMixer) this.animationMixer.update( mixerUpdateDelta );
-// 	}
-// }
+				const character_option = {
+					name: character_file,
+					character: fbx,
+					animationMixer: null,
+					animation: null,
 
-// characterAnimationSceneObject.loadResources();
-// characterAnimationSceneObject.init();
+					init: function() {
+						this.animationMixer = new THREE.AnimationMixer( this.character );
+						this.animation = this.animationMixer.clipAction(this.character.animations[0])
+						this.animationMixer.addEventListener( 'loop', function( e ) {
+							this.animation.paused = true;
+							if(ref.animationLoopCompleteCallback) ref.animationLoopCompleteCallback();
+							this.animationLoopCompleteCallback = null;
+						});
+						this.animation.paused = true;
+						this.animation.play();
+						this.character.visible = false;
+					},
 
-// export default characterAnimationSceneObject;
+					showAndPlay: function() {
+						this.character.visible = true;
+						this.animation.paused = false;
+					},
+
+					stopAndHide: function() {
+						this.animation.stop();
+						this.animation.play();
+						this.animation.paused = true;
+						this.character.visible = false;			
+					}
+				}
+
+				character_option.init();
+
+				ref.characters.push(character_option);
+				ref.scene.add(character_option.character);
+				loadingManager.objectHasBeenLoaded();
+			});	
+		});
+	},
+
+	setActiveCharacterAndPlay: function(name) {
+		for (let i = 0; i < this.character_files.length; i++) {
+			const element = this.character_files[i];
+			if(element.name === element) {
+				this.active_character = element;
+				this.active_character.showAndPlay();
+			}
+		}
+	},
+
+	animate: function() {
+		var mixerUpdateDelta = this.clock.getDelta();
+		if(this.active_character.animationMixer) {
+			this.active_character.animationMixer.update( mixerUpdateDelta );
+		}
+	}
+}
+
+paintedCharacterAnimation.loadResources();
+paintedCharacterAnimation.init();
+
+export default paintedCharacterAnimation;
