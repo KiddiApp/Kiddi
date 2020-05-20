2d3581409ae487f555ec4baee7bad082c87e94d9
FuturisticSpaceCake <thomas.c.hoek@gmail.com>
2020-05-20 16:54:38 +0200

    added styles for the answers and information pages. added extra helper js to accomodate the style

--- js/modules/contentPages/informationpage.js
+++ js/modules/contentPages/informationpage.js
@@ -1,13 +1,16 @@
 import Display from '../helperFunctions.js';
+import {clouds, clouds_manager } from '../clouds_manager.js';
 
 let information_content = {
 	container: document.getElementById("information"),
 	Show(callBack) {
 		Display(true, this.container);
+		clouds_manager.hideClouds(clouds.top_left, clouds.top_right, clouds.mid_left, clouds.mid_right, clouds.bottom_center);
 		if(callBack) callBack();
 	}, 
 	Hide(callBack) {
 		Display(false, this.container);
+		clouds_manager.showClouds(clouds.top_left, clouds.top_right, clouds.mid_left, clouds.mid_right);
 		if(callBack) callBack();
 	}
 }
