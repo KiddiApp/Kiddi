d213a1db13033ff156b5d0d4afb104fa04438876
FuturisticSpaceCake <thomas.c.hoek@gmail.com>
2020-05-25 16:20:29 +0200

    added a nice loading popup. little css changes

--- js/modules/contentLoader.js
+++ js/modules/contentLoader.js
@@ -1,22 +1,24 @@
 import Display from './helperFunctions.js';
+import { ShowPopup, HidePopup } from './popup.js';
 
 let loadingManager = {
 
 	loadingOverlay: document.getElementById("loading_overlay"),
-	toBeLoadedObjectsCount: 6,
+	toBeLoadedObjectsCount: 8,
 	loadedObjectsCount: 0,
 
 	objectHasBeenLoaded: function() { 
 		this.loadedObjectsCount++;
 
-		console.log("AN OTHER OBJECT HAS BEEN LOADED " + this.loadedObjectsCount);
+		// console.log("AN OTHER OBJECT HAS BEEN LOADED " + this.loadedObjectsCount);
 		if(this.loadedObjectsCount >= this.toBeLoadedObjectsCount) {
-			console.log("ALL OBJECTS HAVE BEEN LOADED");
+			// console.log("ALL OBJECTS HAVE BEEN LOADED");
 			this.loadingComplete();
 		}
 	},
 
 	loadingComplete: function() {
+		HidePopup();
 		Display(false, this.loadingOverlay);
 	}
 }
